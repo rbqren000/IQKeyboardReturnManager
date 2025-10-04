c6db99f6b58f7553186afecee34771cc0c5fc48f
hackiftekhar <hack.iftekhar@gmail.com>
2025-10-05 01:30:37 +0530

    Updated Bundle id

--- Package.swift
+++ Package.swift
@@ -14,7 +14,7 @@ let package = Package(
         )
     ],
     dependencies: [
-        .package(url: "https://github.com/hackiftekhar/IQKeyboardCore.git", from: "1.0.7"),
+        .package(url: "https://github.com/hackiftekhar/IQKeyboardCore.git", from: "1.0.8"),
     ],
     targets: [
         .target(name: "IQKeyboardReturnManager",
